c2ed0c31e7a9c31a893bcf8c29c4cbc6ee1445e8
Gabriel Jr Oyarekhua <jroyarekhua@Gabriels-MacBook-Air.local>
2025-05-16 00:12:11 -0400

    current progress

--- Scripts/getStats.py
+++ Scripts/getStats.py
@@ -1,36 +1,46 @@
-# This program pulls player and team data from the NBA API, cleans it,
-# and prepares it for insertion into a PostgreSQL database.
+# This program will pull player data from the nba api and store it in a postgresDB
 
 # Import necessary modules
-from nba_api.stats.static import players  # Module to fetch static player info
+from nba_api.stats.static import players,teams  # Module to fetch static player info
 from pandas import concat         # For data manipulation                        # For PostgreSQL connect
 import time
 from helper import *
-from sqlalchemy import create_engine
 
 
 # Initialize a list to store cleaned season-by-season stats
-seasons = []
+season = []
+team = []
 
 # Fetch active NBA players dictionary
 players_list = players.get_active_players()
+teams_list = teams.get_teams()
 
 # format into a dataframe
 players_frame = getPlayers(players_list)
+teams_frame = getTeams(teams_list)
+
 
 # Extract player IDs to loop over
-id_list = players_frame["id"]
+player_id_list = players_frame["id"]
+team_id_list = teams_frame["id"]
 
-
-# Loop through each player ID to get season averages
-for i in id_list:
-    getSeasonStats(i)
+for i in team_id_list:
+    t_frame = getTeamStats(i) # should return the formatted teams frame to append to the table
+    team.append(t_frame)
+    print(f'team {i} sucessfully added')
     time.sleep(.5)
 
-# Concatenate all players’ season data into one DataFrame
-seasons_frame = concat(seasons, ignore_index=True)
+# Loop through each player ID to get season averages
+# for i in player_id_list:
+#     s_frame = getSeasonStats(i)
+#     season.append(s_frame)
+#     print(f'player {i} sucessfully added')
+#     time.sleep(.5)
 
 
-# Display a preview of the final DataFrame
-print(seasons_frame.head())
-print(seasons_frame.info())
+
+# seasons_frame = concat(season, ignore_index=True)
+teams_frame = concat(team, ignore_index=True)
+# seasons_frame.to_csv("/Users/jroyarekhua/nba_predictions/data/player_season.csv", index=False)
+teams_frame.to_csv("/Users/jroyarekhua/nba_predictions/data/team.csv", index=False)
+
--- Scripts/helper.py
+++ Scripts/helper.py
@@ -4,6 +4,7 @@ from nba_api.stats.static import teams    # Module to fetch static team info
 from nba_api.stats.endpoints import PlayerDashboardByYearOverYear  # To get season-by-season stats
 import pandas as pd                       # For data manipulation
 import time
+from nba_api.stats.endpoints import TeamDashboardByGeneralSplits
 
 def getPlayers(players_dict):
     # Reformat list of player dicts into a list of single-row DataFrames
@@ -38,7 +39,6 @@ def getSeasonStats(player_id):
 
             # rename grouo value to season_id
             player_frame = player_frame.rename(columns={'group_value': 'season_id'})
-            print(player_frame.columns)
 
             # attach season start and end columns
             player_frame["season_start"] = player_frame["season_id"].str[:4].astype(int)
@@ -59,10 +59,34 @@ def getSeasonStats(player_id):
         # Only executes if all retry attempts fail
         print(f"player {player_id} failed after 3 attempts")
 
-def getTeams(teams_dict):
-    pass
+def getTeams(teams_list):
+    teams_frame = []
+    for i in range(len(teams_list)):
+        formatted_dict = {key:[val] for key, val in teams_list[i].items()}
+        df = pd.DataFrame.from_dict(formatted_dict)
+        teams_frame.append(df)
+    teams_frame = pd.concat(teams_frame, ignore_index=True)
+    return teams_frame
+
+
 
 def getTeamStats(team_id):
-    pass
+    retries = 3
+    for attempt in range(retries):
+        try:
+            # logic to retrive stats for an individual team
+            res = TeamDashboardByGeneralSplits(team_id).get_dict()['resultSets'][0]
+            team_frame = pd.DataFrame(res['rowSet'], columns=res['headers'])
+            print(team_frame.shape)
+            print(team_frame.head())
+            team_frame = team_frame.iloc[:,1:29]
+            team_frame.columns = team_frame.columns.str.lower() # make all columns lower case
+            team_frame.rename(columns={'group_value': 'team_id'}, inplace=True)  # rename group value
+            return team_frame
+        except Exception as e:
+            print(f"Attempt {attempt + 1} failed for team {team_id}")
+            print(e)
+            time.sleep(2)
+    raise Exception(f"Attempt failed for team {team_id}")
 
 
